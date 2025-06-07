654b3a058ebfada9ec7ad591ad531648268d1e7e
aJISUa <103941235+aJISUa@users.noreply.github.com>
2025-06-08 01:55:52 +0900

    Update AppointmentRecordGUI.java

--- source/AppointmentRecordGUI.java
+++ source/AppointmentRecordGUI.java
@@ -100,7 +100,7 @@ public class AppointmentRecordGUI extends JFrame {
             String rec = recordField.getText().trim();
 
             boolean result = AppointmentRecordsql.insertRecord(userId, pid, presc, diag, rec);
-            JOptionPane.showMessageDialog(this, result ? "등록 완료!" : "등록 실패!");
+            JOptionPane.showMessageDialog(this, result ? "등록 완료!" : "등록 실패 - 알레르기 충돌 확인");
         }
     }
 
@@ -125,7 +125,7 @@ public class AppointmentRecordGUI extends JFrame {
             String rec = recordField.getText();
 
             boolean result = AppointmentRecordsql.updateRecord(id, diag, presc, rec);
-            JOptionPane.showMessageDialog(this, result ? "수정 완료!" : "수정 실패 또는 알레르기 충돌!");
+            JOptionPane.showMessageDialog(this, result ? "수정 완료" : "수정 실패 - 알레르기 충돌 확인");
         }
     }
 
@@ -134,7 +134,7 @@ public class AppointmentRecordGUI extends JFrame {
         if (idStr != null) {
             int id = Integer.parseInt(idStr);
             boolean result = AppointmentRecordsql.deleteRecord(id);
-            JOptionPane.showMessageDialog(this, result ? "삭제 완료!" : "삭제 실패!");
+            JOptionPane.showMessageDialog(this, result ? "삭제 완료" : "삭제 실패");
         }
     }
 
